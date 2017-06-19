dc0369c1a6466c05238a25897b68d6730af970e1
sergiimeleshko <sergey.meleshko@thomascookonline.com>
2017-06-19 16:35:01 +0300

    Added config-server-mcs module. Switched microservices from local config to remote config via Config Server concept.

--- search-statistic-mcs/src/main/java/sir/mllmn/search/controller/StatisticController.java
+++ search-statistic-mcs/src/main/java/sir/mllmn/search/controller/StatisticController.java
@@ -1,6 +1,7 @@
 package sir.mllmn.search.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import sir.mllmn.search.domain.Search;
 import sir.mllmn.search.repository.ISearchRepository;
@@ -14,12 +15,21 @@ public class StatisticController {
 	@Autowired
 	private ISearchRepository searchRepository;
 
+	@Value("${application.view-name}")
+	private String appName;
+
 	private int searchIdCounter;
 
 	@GetMapping
 	public List<Search> getSearches() {
 		return searchRepository.getSearches();
 	}
+
+	@GetMapping("info")
+	public String info() {
+		return appName;
+	}
+
 
 	@GetMapping("search/{id}")
 	public Search getSearch(@PathVariable("id") int id) {
