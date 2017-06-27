10caf66c06515f965600f93313eb9d8947ae937d
sergiimeleshko <sergey.meleshko@thomascookonline.com>
2017-06-27 19:03:58 +0300

    Provided some custom configuration for Ribbon client

--- library-manager-mcs/src/main/java/sir/mllmn/books/startup/LibraryManagerMcsApplication.java
+++ library-manager-mcs/src/main/java/sir/mllmn/books/startup/LibraryManagerMcsApplication.java
@@ -3,11 +3,14 @@ package sir.mllmn.books.startup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
+import sir.mllmn.books.config.RibbonConfiguration;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("sir.mllmn.books")
+@RibbonClient(name = "search-statistic-app", configuration = RibbonConfiguration.class)
 public class LibraryManagerMcsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagerMcsApplication.class, args);
