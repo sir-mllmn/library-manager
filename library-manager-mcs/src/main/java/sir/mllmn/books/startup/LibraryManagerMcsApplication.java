835491ebfaa9a3d74d8f3fbe2c2ce79a4247e05a
sergiimeleshko <sergey.meleshko@thomascookonline.com>
2017-06-30 10:20:40 +0300

    Replaced pure usage of RestTemplate for invocation of search-statistic-app with FeignClient

--- library-manager-mcs/src/main/java/sir/mllmn/books/startup/LibraryManagerMcsApplication.java
+++ library-manager-mcs/src/main/java/sir/mllmn/books/startup/LibraryManagerMcsApplication.java
@@ -3,14 +3,13 @@ package sir.mllmn.books.startup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import sir.mllmn.books.config.RibbonConfiguration;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("sir.mllmn.books")
-@RibbonClient(name = "search-statistic-app", configuration = RibbonConfiguration.class)
+@EnableFeignClients(basePackages = "sir.mllmn.books.client")
 public class LibraryManagerMcsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagerMcsApplication.class, args);
